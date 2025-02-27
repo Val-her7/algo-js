7284a89c2dd6c371e39764d3a401dc838ffb07f2
Valher7 <Valentin.herman.pro@gmail.com>
2025-02-27 15:37:59 +0100

    Add exercice 3.0

---
+++ 3.0.js
@@ -0,0 +1,4 @@
+let learners = ["Antoine", "Arsenia", "Manu", "Jordan", "Hugo", "Valentin", "Stephan", "Liana", "Kamal", "Martin"];
+for (let learner of learners) {
+    console.log(learner + " is a learner from the class!");
+}
