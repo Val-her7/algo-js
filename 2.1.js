c4f007ac41cd59e9abe014dc8ec65c10376e321e
Valher7 <Valentin.herman.pro@gmail.com>
2025-02-27 12:56:40 +0100

    Exercice 2.1 improve

--- 2.1.js
+++ 2.1.js
@@ -1,6 +1,14 @@
-let age = Number(prompt("How old are you?"));
-if (age >= 18){
-    console.log("You are an adult!");
+const prompt = require("prompt-sync")();
+let age = prompt("Enter your age: ");
+console.log(age);
+if (isNaN(Number(age))){
+    console.log("This is not a number...");
+} else if (age === null){
+    console.log("You did not enter anything...");
 } else {
-    console.log("You are not yet an adult!");
+    if (Number(age) >= 18){
+        console.log("You are an adult!");
+    } else {
+        console.log("You are not yet an adult!");
+    }
 }
