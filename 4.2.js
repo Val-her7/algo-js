17e9ed59fb9c12d086765229d9406ac7de3dcb6c
Valher7 <Valentin.herman.pro@gmail.com>
2025-02-28 10:20:14 +0100

    Add exercice 4.2

---
+++ 4.2.js
@@ -0,0 +1,8 @@
+/**
+ * Returns a random integer between 1 and 10
+ * @returns {number} - The random integer between 1 and 10
+ */
+function rand10(){
+    return Math.ceil(Math.random()*10);
+}
+console.log(rand10());
